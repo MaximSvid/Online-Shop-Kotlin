50fd3b90faa4d2eebe93b8ecb4de926e16e62a9a
Maxim Svidrak <maximsvid@gmail.com>
2024-09-03 13:14:25 +0200

    initial commit
    StartFragment

---
+++ app/src/main/java/com/example/onlineshopprojectkotlin/ui/StartFragment.kt
@@ -0,0 +1,25 @@
+package com.example.onlineshopprojectkotlin.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.onlineshopprojectkotlin.R
+import com.example.onlineshopprojectkotlin.databinding.FragmentStartBinding
+
+class StartFragment : Fragment() {
+
+    private lateinit var binding: FragmentStartBinding
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentStartBinding.inflate(layoutInflater)
+        // Inflate the layout for this fragment
+        return binding.root
+    }
+
+}
