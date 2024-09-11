b57dc3b87ed53a24bab60e0db7933744e7c93914
Maxim Svidrak <maximsvid@gmail.com>
2024-09-11 11:40:00 +0200

    Moshi...

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -80,12 +80,14 @@ dependencies {
     implementation("com.squareup.okhttp3:logging-interceptor:4.10.0")
     implementation("com.squareup.okhttp3:okhttp:4.10.0")
 
-    //Moshi
-    implementation("com.squareup.moshi:moshi:1.15.1")
-    implementation("com.squareup.moshi:moshi-kotlin:1.15.1")
     //Retrofit
     implementation ("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-moshi:2.9.0")
+
+    //Moshi
+    implementation("com.squareup.moshi:moshi:1.15.1")
+    implementation("com.squareup.moshi:moshi-kotlin:1.15.1")
+
 
     // KSP for Moshi code generation
 //    ksp("com.squareup.moshi:moshi-kotlin-codegen:1.15.1")
